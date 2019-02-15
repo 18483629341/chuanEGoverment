6947e0f728c29c7dad1d808dd11d6cbcff498183
lisa091500 <714402934@qq.com>
2019-02-15 09:21:35 +0800

    scroll mostly over

---
+++ .svn/pristine/b6/b6cb27e30d59206417f327f946c66cbe460fc583.svn-base
@@ -0,0 +1,8 @@
+$(function () {
+	//获取文本的行高，并获取文本的高度，假设我们规定的行数是五行，那么对超过行数的部分进行限制高度，并加上省略号
+	onlyTwoLine('.li-heading');
+	$('#myTabs a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	})
+})
--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 $(function () {
 	//获取文本的行高，并获取文本的高度，假设我们规定的行数是五行，那么对超过行数的部分进行限制高度，并加上省略号
 	onlyTwoLine('.li-heading');
-	$('#myTabs a').click(function (e) {
+	$('.selfTab a').click(function (e) {
+		console.log(1);
 		e.preventDefault()
-		$(this).tab('show')
+		$(this).tab('show');
+		onlyTwoLine('.li-heading');
 	})
+	$('.selfTab a').mouseover(function (e) {
+		console.log(1);
+		e.preventDefault()
+		$(this).tab('show');
+		onlyTwoLine('.li-heading');
+	})
+	$("#content-1").mCustomScrollbar({
+		theme:"minimal"
+	});
 })
